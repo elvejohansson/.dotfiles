c70665c19960c5b4546540d2e3b20e421ee1b663
Elve <elve.mikael@proton.me>
2024-09-08 15:02:51 +0200

    neovim options, alacritty background color

---
+++ nvim/.config/nvim/lua/elve/init.lua
@@ -0,0 +1,7 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+vim.cmd.colorscheme("quiet")
+
+require("elve.keymaps")
+require("elve.options")
---
+++ nvim/.config/nvim/lua/elve/keymaps.lua
@@ -0,0 +1,10 @@
+vim.keymap.set("n", "<up>", "<nop>")
+vim.keymap.set("n", "<down>", "<nop>")
+vim.keymap.set("n", "<left>", "<nop>")
+vim.keymap.set("n", "<right>", "<nop>")
+vim.keymap.set("i", "<up>", "<nop>")
+vim.keymap.set("i", "<down>", "<nop>")
+vim.keymap.set("i", "<left>", "<nop>")
+vim.keymap.set("i", "<right>", "<nop>")
+
+vim.keymap.set("n", "<leader>fe", vim.cmd.Ex)
