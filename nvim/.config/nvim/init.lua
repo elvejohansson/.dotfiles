1891c2061c765deaf4779f18b1cbe8ebdc91684f
Elve <elve.mikael@proton.me>
2024-09-14 21:38:26 +0200

    move neovim config into single file

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,1 +1,155 @@
-require("elve")
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
+vim.api.nvim_set_option("clipboard", "unnamed")
+
+vim.keymap.set("n", "<up>", "<nop>")
+vim.keymap.set("n", "<down>", "<nop>")
+vim.keymap.set("n", "<left>", "<nop>")
+vim.keymap.set("n", "<right>", "<nop>")
+vim.keymap.set("i", "<up>", "<nop>")
+vim.keymap.set("i", "<down>", "<nop>")
+vim.keymap.set("i", "<left>", "<nop>")
+vim.keymap.set("i", "<right>", "<nop>")
+
+vim.keymap.set("n", "<leader>fe", vim.cmd.Ex)
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.termguicolors = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.incsearch = true
+vim.opt.hlsearch = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.scrolloff = 999
+
+vim.opt.signcolumn = "yes"
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+    spec = {
+        "tpope/vim-sleuth",
+
+        {
+            "mellow-theme/mellow.nvim",
+            lazy = false,
+            priority = 1000,
+        },
+        {
+            "nvim-treesitter/nvim-treesitter",
+            build = ":TSUpdate",
+            opts = {
+                ensure_installed = { "c", "lua", "vimdoc", "json", "yaml" },
+                auto_install = true,
+                highlight = {
+                    enable = true,
+                },
+                indent = {
+                    enable = true,
+                },
+            },
+        },
+        {
+            "nvim-telescope/telescope.nvim",
+            tag = "0.1.8",
+            dependencies = {
+                "nvim-lua/plenary.nvim",
+            },
+            config = function()
+                local builtin = require("telescope.builtin")
+                vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+                vim.keymap.set("n", "<leader>fs", builtin.live_grep, {})
+                vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+                vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+            end
+        },
+        {
+            "lukas-reineke/indent-blankline.nvim",
+            main = "ibl",
+            opts = {
+                indent = {
+                    char = "▏",
+                    smart_indent_cap = true,
+                },
+                scope = {
+                    enabled = true,
+                },
+            }
+        },
+        {
+            "folke/todo-comments.nvim",
+            dependencies = {
+                "nvim-lua/plenary.nvim",
+            },
+            opts = {},
+        },
+        {
+            "lukas-reineke/virt-column.nvim",
+            opts = {
+                enabled = true,
+                char = "┃",
+                virtcolumn = "100",
+            },
+        },
+        {
+            "f-person/git-blame.nvim",
+            opts = {
+                enabled = true,
+                delay = 175,
+            },
+        },
+        {
+            "nvim-lualine/lualine.nvim",
+            dependencies = {
+                "kyazdani42/nvim-web-devicons",
+            },
+            opts = {},
+        },
+        {
+            "windwp/nvim-autopairs",
+            event = "InsertEnter",
+            config = true
+        }
+    },
+    checker = { enabled = true },
+})
+
+vim.cmd([[colorscheme mellow]])
